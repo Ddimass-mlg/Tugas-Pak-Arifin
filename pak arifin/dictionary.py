0fabb0e8d46e5d62ec787aaf06d4c1dc48a921e1
Ddimass-mlg <samidayra7@gmail.com>
2025-12-10 21:43:36 +0700

    dictionary.py

---
+++ pak arifin/dictionary.py
@@ -0,0 +1,7 @@
+daftar_harga = {'ihsan' : 900,'kendra' : 100, 'dimas' : 50}
+daftar_harga['ihsan']
+print(daftar_harga['ihsan'])
+list(daftar_harga.keys())
+print(daftar_harga.keys())
+list(daftar_harga.values())
+print(daftar_harga.values())
